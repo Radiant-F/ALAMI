fcd629b92286a23c30e873371137cea6adbd2776
RadiantF <radiantfadilah0@gmail.com>
2021-02-17 06:46:22 +0700

    initial commit

---
+++ src/components/Intro.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+export default class Intro extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Intro </Text>
+      </View>
+    );
+  }
+}
