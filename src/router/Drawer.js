7f2396a79e7d2fe898e94725861f002b72067bd8
RadiantF <radiantfadilah0@gmail.com>
2021-02-17 06:45:55 +0700

    update drawer

---
+++ src/router/Drawer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import Dashboard from '../screen/Dashboard';
+import Content from '../components/Drawer';
+
+const Slide = createDrawerNavigator();
+const Drawer = () => {
+  return (
+    <Slide.Navigator drawerContent={(props) => <Content {...props} />}>
+      <Slide.Screen name="Splash" component={Dashboard} />
+    </Slide.Navigator>
+  );
+};
+
+export default Drawer;
