0655e673783c7fd61038707ce78b77edad17b6a5
RadiantF <radiantfadilah0@gmail.com>
2021-02-19 17:43:49 +0700

    add navigation

--- src/router/Navigator.js
+++ src/router/Navigator.js
@@ -3,6 +3,9 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import Splash from '../components/Splash';
 import Drawer from './Drawer';
+import Login from '../auth/Login';
+import Intro from '../components/Intro';
+import Register from '../auth/Register';
 
 const Stack = createStackNavigator();
 const Navigator = () => {
@@ -10,6 +13,9 @@ const Navigator = () => {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Splash" headerMode="none">
         <Stack.Screen name="Splash" component={Splash} />
+        <Stack.Screen name="Intro" component={Intro} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
         <Stack.Screen name="Dashboard" component={Drawer} />
       </Stack.Navigator>
     </NavigationContainer>
