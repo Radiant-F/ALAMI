446c4d9cd72839ca0545ea6b1d4602dd80ffc39a
RadiantF <radiantfadilah0@gmail.com>
2021-02-19 17:43:19 +0700

    mortal intro

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,18 +1,44 @@
+import AsyncStorage from '@react-native-community/async-storage';
 import React, {Component} from 'react';
-import {Button, Text, View} from 'react-native';
+import {ActivityIndicator, Button, StyleSheet, Text, View} from 'react-native';
 
 export default class Splash extends Component {
+  componentDidMount() {
+    setTimeout(() => {
+      AsyncStorage.getItem('token')
+        .then((value) => {
+          if (value) {
+            this.props.navigation.replace('Dashboard', {screen: 'Dashboard'});
+          } else {
+            AsyncStorage.getItem('first')
+              .then((value) => {
+                if (value) {
+                  this.props.navigation.replace('Login');
+                } else {
+                  this.props.navigation.replace('Intro');
+                }
+              })
+              .catch((err) => console.log(err));
+          }
+        })
+        .catch((err) => console.log(err));
+    }, 2000);
+  }
+
   render() {
     return (
-      <View>
-        <Button
-          title="Navigasi"
-          onPress={() =>
-            this.props.navigation.navigate('Dashboard', {screen: 'Dashboard'})
-          }
-        />
-        <Text> Splash </Text>
+      <View style={styles.mainView}>
+        <Text> Memuat.. </Text>
+        <ActivityIndicator color="green" size="large" />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  mainView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
