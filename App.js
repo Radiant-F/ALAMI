95c1210178bcf55ce9cb6513078759ebb6ea9a78
RadiantF <radiantfadilah0@gmail.com>
2021-02-19 17:22:34 +0700

    add provider for redux

--- App.js
+++ App.js
@@ -1,9 +1,16 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
 import Navigator from './src/router/Navigator.js';
+import Intro from './src/components/Intro';
+import {Provider} from 'react-redux';
+import {store} from './src/redux/store/index';
 
 export default class App extends Component {
   render() {
-    return <Navigator />;
+    return (
+      <Provider store={store}>
+        <Navigator />
+      </Provider>
+    );
   }
 }
